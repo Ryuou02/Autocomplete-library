08138a298a45b9f8c3aea98a6ba0900bf8f32bd0
Nikhil Tom Jose <133224167+Ryuou02@users.noreply.github.com>
2025-03-12 11:02:39 +0530

    optimized autocomplete.cpp

--- autocomplete.cpp
+++ autocomplete.cpp
@@ -1,144 +1,98 @@
-#include "autocomplete.h"
-#include<string>
+/****************************************
+*
+* Autocomplete library
+* Developed by Ryuou02
+* https://github.com/Ryuou02
+*
+*****************************************/
 
-void autocomplete::deleteguess(string guess)
-{
-	if (!guess.empty()) {
-		for (int i = 0; i < guess.size(); i++)
-			_putch(32);
-		for (int i = 0; i < guess.size(); i++)
-			_putch(8);
-	}
+#include "autocomplete.h"
+#include<chrono>
+#include<algorithm>
+#include<string>
+#include<fstream>
+
+void wordToACbuffer(autocomplete& obj, const std::string& sentence) {	// get the most recent word from sentence and add it to autocomplete buffer
+	int cursor = sentence.size() - 1;
+	while (cursor >= 0 && sentence[cursor] >= 'a' && sentence[cursor] <= 'z')
+		cursor--;
+	cursor++;
+	while (cursor < sentence.size())
+		obj.addChar(sentence[cursor++]);
 }
 
 autocomplete::autocomplete(string* wordset, int datasize) {
-	isnew = false;
-	words = wordset;
 	wordnum = datasize;
+	std::ofstream filewrite("logs.txt");
+	this->wordset = vector<string>(datasize);
+	for (int i = 0; i < datasize; i++) {
+		this->wordset[i] = wordset[i];
+		filewrite << wordset[i] << '\n';
+	}
+	filewrite.close();
+
 }
 
-autocomplete::autocomplete(const char* wordset[], int wordsnumber)
+autocomplete::autocomplete(const char* wordset[], int wordsnumber) 
 {
-	isnew = true;
-	words = new string[wordsnumber];
+	ofstream filewrite;
+	filewrite.open("logs.txt");
 	wordnum = wordsnumber;
+	this->wordset = vector<string>(wordsnumber);
+
 	for (int i = 0; i < wordsnumber; i++) {
-		//std::cout << wordset[i] << '\n';
-		words[i] = string(wordset[i]);
+		this->wordset[i] = string(wordset[i]);
+		filewrite << wordset[i] << '\n';
+	}
+	std::sort(this->wordset.begin(), this->wordset.end());
+	filewrite.close();
+}
+
+void autocomplete::addChar(char in){
+	// normalisation
+	if ('A' <= in && in <= 'Z')
+		in += 32;
+	if (in >= 'a' && in <= 'z') {
+		buffer.push_back(in);
+		getGuess();
 	}
 }
-autocomplete::~autocomplete()
-{
-	if(isnew)
-		delete[] words;
-}
-string autocomplete::input()
-{
-	wchar_t in = 0;
-	string input = "";
-	string guess = "";
 
-	int pos = -1;
+bool autocomplete::addToWordset(string word)
+{
 	int beg = 0, end = wordnum;
-
-	while (in != 13 && in != ' ') {
-		while (!_kbhit());
-		in = _getch();
-
-		if (in >= 33 && in <= 127) {	// is not special character
-			_putch(in);
-			input.push_back(in);
-			pos++;
-			
-			for (; beg < end; beg++) {
-				if (pos + 1 < words[beg].size() && (words[beg][pos] == in)) {
-					break;
-				}
-			}
-			if (beg < end) {
-				for (int i = beg; i < end; i++) {
-					if (pos < words[i].size() && words[i][pos] != in) {
-						end = i;
-						break;
-					}
-				}
-				deleteguess(guess);
-				guess = words[beg].substr(pos + 1, words[beg].size() - pos);
-				std::cout << "\033[2m";
-				_cputs(guess.c_str());
-
-				for (int i = 0; i < guess.size(); i++)
-					_putch(8);
-
-				std::cout << "\033[0m";;
-			}
-			else {
-				// word not found
-				deleteguess(guess);
-				while (in != 13 && in != ' ') {
-					in = _getch();
-					if (in >= 33 && in <= 127) {
-						_putch(in);
-						input.push_back(in);
-					}
-					else {
-						switch (in) {
-						case 8:
-							if (!input.empty())
-								input.pop_back();
-							else
-								return this->input();
-							_putch(8);
-							_putch(32);
-							_putch(8);
-							break;
-						case 224:
-							_getch();
-						}
-					}
-				}
-				input.push_back(in);
-				return input;
-			}
+	int mid = (beg + end) / 2;
+	while (end > beg) {
+		if (wordset[mid] > word) {
+			end = mid - 1;
 		}
 		else {
-			switch (in) {
-			case 8:	// backspace
-				if (!input.empty())
-					input.pop_back();
-				else
-					return "\b";
-
-				if (--pos == -1) {
-					beg = 0;
-					end = wordnum;
-				}
-				deleteguess(guess);
-
-				_putch(8);
-				_putch(32);
-				_putch(8);
-				break;
-			case '\t':
-				if(!guess.empty()) {
-					_cputs(guess.c_str());	//no need to delete guess
-					input += guess;
-					pos += guess.size();
-				}
-				break;
-			case 224:
-				// special
-				if (input.empty()) {
-					input.push_back(224);
-					return input;
-				}
-				else
-					_getch();
-				break;
-			}
+			beg = mid + 1;
 		}
+		mid = (end + beg) / 2;
 	}
-	deleteguess(guess);
-	input.push_back(in);
-	return input;
+	
+	if (wordset[mid] < word && wordset[mid + 1] > word) {
+		wordset.insert(wordset.begin() + mid, word);
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
+
+// getGuess function will return the guess of the word number generated for the corresponding word in buffer
+// in case of no guess, guessExists variable is set to false and no guess is returned.
+// else guessExists is true and guess is returned.
+// in case of buffer size not changing, empty string is returned.
+// in case of buffer being smaller, i.e. characters in buffer were deleted without use of backspace() function, then an exception is thrown
+void autocomplete::getGuess()
+{
+	int b = lower_bound(wordset.begin(), wordset.end(), buffer) - wordset.begin();
+	buffer[buffer.size() - 1] += 1;
+	int e = upper_bound(wordset.begin(), wordset.end(), buffer) - wordset.begin();
+	if (b < e)
+		guess = b;
+	else guess = -1;
+	buffer[buffer.size() - 1] -= 1;
 }
